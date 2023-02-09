41e394ca083ea7c1bc12e83ed109e64429c9b0d9
Xingang Wang <wxg.xingang@outlook.com>
2023-02-09 13:38:55 +0800

    env: add script for apt install
    Signed-off-by: Xingang Wang <wxg.xingang@outlook.com>

---
+++ env/apt.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# basic
+sudo apt install -y vim git tldr zsh htop sshfs curl python python3 cmake
+
+# build
+sudo apt install -y build-essential bison flex binutils libelf-dev
+
+# code
+
+sudo apt install -y python python3 cmake python3-dev pip python3-pip
